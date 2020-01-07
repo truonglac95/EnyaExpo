6f61e49dc52121474e3e8c590d0e6a8ead22597a
cby3149 <46272347+cby3149@users.noreply.github.com>
2020-01-06 17:28:31 -0800

    Updated urls

--- EnyaSDK/EnyaSMC/src/auth.js
+++ EnyaSDK/EnyaSMC/src/auth.js
@@ -1,5 +1,5 @@
-module.exports.SECURE_PRS_CHANNEL_1 = 'https://enya-token.blockdoc.com/';
-module.exports.SECURE_PRS_CHANNEL_2 = 'https://enya-beavertriple.blockdoc.com/';
-module.exports.SECURE_PRS_CHANNEL_3 = 'https://enya-smc.blockdoc.com/';
-module.exports.SECURE_PRS_CHANNEL_4 = 'https://enya-uuid.blockdoc.com/';
+module.exports.SECURE_PRS_CHANNEL_1 = 'https://api-token.enya.ai/';
+module.exports.SECURE_PRS_CHANNEL_2 = 'https://api-beavertriple.enya.ai/';
+module.exports.SECURE_PRS_CHANNEL_3 = 'https://api-smc.enya.ai/';
+module.exports.SECURE_PRS_CHANNEL_4 = 'https://api-server.enya.ai/';
 module.exports.SECURE_KEY = 'kj925pOnpg3C2m6GeDxh55kW8g8dHkoD5kx7oSlr';
